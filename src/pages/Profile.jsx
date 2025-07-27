2cdf4b2640714655ba737a8b543a82124c41d24d
basttian <sebastianjof@gmail.com>
2025-07-27 12:35:23 -0300

    fix-profile-page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,19 +1,24 @@
 import { useAuth } from '../context/AuthContext';
-import { Link } from 'react-router';
+import { Link, Navigate } from 'react-router';
 
 export const Profile = () => {
 
     const { user } = useAuth();
+
+    if (!user) {
+        // Si no hay usuario, redirigir a la página de login
+        return <Navigate to="/login" />;
+    }
 
     return (
         <div>
         <h1>Profile</h1>
         {user && (
             <div>
-            <Link to='/Dashboard'>Escritorio</Link>
-            <h2>Bienvenido, {user.name}</h2>
-            <p>Email: {user.email}</p>
-            <img src={user.picture} alt="Foto de perfil" />
+                <Link to='/dashboard'>Escritorio</Link>
+                <h2>Bienvenido, {user.name}</h2>
+                <p>Email: {user.email}</p>
+                <img src={user.picture} alt="Foto de perfil" />
             </div>
         )}
         </div>
